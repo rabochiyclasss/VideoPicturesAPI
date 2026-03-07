eafaa8fc3491cbf303751d2610609fb1b641e21a
rabochiyclasss <20budkoivan02@gmail.com>
2026-03-07 22:47:13 +0100

    get post by id was added

--- app/app.py
+++ app/app.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-@app.get("/hello-world")
-def hello_world():
-    return {"message": "Hello World"}
+text_post = {1: {"title": "New Title", "content": "New Content"}}
+
+@app.get("/posts")
+def get_all_posts():
+    return text_post
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    if id not in text_post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return text_post.get(id)
