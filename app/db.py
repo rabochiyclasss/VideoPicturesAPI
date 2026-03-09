da02e98b2ae7f1e18d913d3d19976a4fd16d0dcd
rabochiyclasss <20budkoivan02@gmail.com>
2026-03-09 21:49:59 +0100

    minor bugs were fixed

--- app/db.py
+++ app/db.py
@@ -6,11 +6,14 @@ from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
-import datetime
+from datetime import datetime, timezone
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
-class Post(DeclarativeBase):
+class Base(DeclarativeBase):
+    pass
+
+class Post(Base):
     __tablename__ = "posts"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
@@ -18,7 +21,7 @@ class Post(DeclarativeBase):
     url = Column(String, nullable=False)
     file_type = Column(String, nullable=False)
     file_name = Column(String, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    created_at = Column(DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc))
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
@@ -26,7 +29,7 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 #it will create all tables from classes which inherits DeclarativeBase
 async def create_db_and_tables():
     async with engine.begin() as conn:
-        await conn.run_sync(DeclarativeBase.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 #it is going to get a session which will allow us to actually use the DB and write & read from it asynchronously
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
