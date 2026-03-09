8ee2a8d21e333a03648f015fc205444d2a9f97aa
rabochiyclasss <20budkoivan02@gmail.com>
2026-03-09 21:35:49 +0100

    preparation of DB was finished, only User table is missing

--- app/db.py
+++ app/db.py
@@ -19,3 +19,17 @@ class Post(DeclarativeBase):
     file_type = Column(String, nullable=False)
     file_name = Column(String, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+#it will create all tables from classes which inherits DeclarativeBase
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(DeclarativeBase.metadata.create_all)
+
+#it is going to get a session which will allow us to actually use the DB and write & read from it asynchronously
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
